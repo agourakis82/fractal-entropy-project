e4fb4f2e3135cac5e9e57183b6fefa4c95901e11
Demetrios Agourakis <demetrios@agourakis.med.br>
2025-07-21 11:31:45 -0300

    Implement Monte Carlo and fractal dimension estimators (alpha, beta params)

--- code/box_dimension.py
+++ code/box_dimension.py
@@ -1,4 +1,12 @@
 import networkx as nx, numpy as np
+def box_count_dimension(G, q=1):
+    degrees = np.array([d for _, d in G.degree()])
+    hist, bins = np.histogram(degrees, bins='auto', density=True)
+    p = hist[hist > 0]
+    if q == 1:
+        return -np.sum(p * np.log(p)) / np.log(bins[-1]-bins[-2])
+    else:
+        return np.log(np.sum(p**q)/(bins[-1]-bins[-2]))/(1-q)
 
 def box_count_dimension(G, q=1, epsilons=None):
     """
