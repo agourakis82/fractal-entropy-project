5fd5b9bea6e3077dd1dc6481ea94151495e3ff6f
Demetrios Agourakis <demetrios@agourakis.med.br>
2025-07-21 08:06:42 -0300

    Create box_dimension.py
    Add box‑counting stub for fractal dimensions

---
+++ code/box_dimension.py
@@ -0,0 +1,16 @@
+import networkx as nx, numpy as np
+
+def box_count_dimension(G, q=1, epsilons=None):
+    """
+    Estimate multifractal dimension D_q for an ego graph G.
+    Parameters
+    ----------
+    G : networkx.Graph
+    q : float
+    epsilons : iterable, box sizes (defaults to logspace)
+    Returns
+    -------
+    D_q : float
+    """
+    # TODO: implement
+    pass
