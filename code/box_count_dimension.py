d5f04e73c8c99d171fa800b820f59d698d9d2585
Demetrios Agourakis <demetrios@agourakis.med.br>
2025-07-23 08:13:51 -0300

    Add bootstrap CI to fractal dimension

--- code/box_count_dimension.py
+++ code/box_count_dimension.py
@@ -1,24 +1,25 @@
-import networkx as nx, numpy as np
-def box_count_dimension(G, q=1):
+import numpy as np
+def box_count_dimension(G, q=1, bootstrap=200):
     degrees = np.array([d for _, d in G.degree()])
-    hist, bins = np.histogram(degrees, bins='auto', density=True)
+    hist, bins = np.histogram(degrees, bins="auto", density=True)
     p = hist[hist > 0]
+    delta = bins[1] - bins[0]
     if q == 1:
-        return -np.sum(p * np.log(p)) / np.log(bins[-1]-bins[-2])
+        D1 = -np.sum(p * np.log(p)) / np.log(delta)
     else:
-        return np.log(np.sum(p**q)/(bins[-1]-bins[-2]))/(1-q)
+        D1 = np.log(np.sum(p**q) / delta) / (1 - q)
 
-def box_count_dimension(G, q=1, epsilons=None):
-    """
-    Estimate multifractal dimension D_q for an ego graph G.
-    Parameters
-    ----------
-    G : networkx.Graph
-    q : float
-    epsilons : iterable, box sizes (defaults to logspace)
-    Returns
-    -------
-    D_q : float
-    """
-    # TODO: implement
-    pass
+    # bootstrap CI
+    boots = []
+    rng = np.random.default_rng(42)
+    for _ in range(bootstrap):
+        sample = rng.choice(degrees, size=len(degrees), replace=True)
+        h, b = np.histogram(sample, bins=bins, density=True)
+        pp = h[h > 0]
+        if q == 1:
+            d1 = -np.sum(pp * np.log(pp)) / np.log(delta)
+        else:
+            d1 = np.log(np.sum(pp**q) / delta) / (1 - q)
+        boots.append(d1)
+    ci = np.percentile(boots, [2.5, 97.5])
+    return D1, ci[0], ci[1]
